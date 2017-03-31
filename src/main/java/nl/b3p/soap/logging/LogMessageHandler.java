7dda03a61d4c7f6476770e79d736be038e18c50a
Mark Prins <markprins@b3partners.nl>
2017-03-31 17:37:29 +0200

    voeg een logger toe
    zodat soap payload en headers in webapps middels log4j of commons-logging kunnen worden gelogd met trace level en niet naar system.out gaan

---
+++ src/main/java/nl/b3p/soap/logging/LogMessageHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2017 B3Partners B.V.
+ */
+package nl.b3p.soap.logging;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+import javax.xml.namespace.QName;
+import javax.xml.soap.MimeHeader;
+import javax.xml.soap.SOAPException;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 'TRACE' level logging van de soap berichten.
+ *
+ * @author mprins
+ */
+public class LogMessageHandler implements SOAPHandler<SOAPMessageContext> {
+
+    private static final Log LOG = LogFactory.getLog(LogMessageHandler.class);
+
+    @Override
+    public Set<QName> getHeaders() {
+        return Collections.EMPTY_SET;
+    }
+
+    @Override
+    public boolean handleMessage(SOAPMessageContext context) {
+        try {
+            if (LOG.isTraceEnabled()) {
+                Iterator<MimeHeader> i = context.getMessage().getMimeHeaders().getAllHeaders();
+                while (i.hasNext()) {
+                    MimeHeader h = i.next();
+                    LOG.trace("header: " + h.getName() + "" + h.getValue());
+                }
+
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                context.getMessage().writeTo(bos);
+                LOG.trace("bericht: " + bos.toString("UTF-8"));
+            }
+        } catch (SOAPException | IOException ex) {
+            LOG.trace(ex);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean handleFault(SOAPMessageContext context) {
+        return true;
+    }
+
+    @Override
+    public void close(MessageContext context) {
+    }
+}
